79487aab820de4d21e98edadfef8fd79395e962a
Michael <85833344+MichaelTheChef@users.noreply.github.com>
2024-01-02 13:06:48 -0500

    Transfers - Molex AI Model, updated prediction responses

--- transfers/training/predict.py
+++ transfers/training/predict.py
@@ -6,7 +6,7 @@ class Predict:
         self.recent_file_df = None
         self.recent_network_df = None
 
-    def add_dataframe(self, df: pd.DataFrame, transfer_type: str):
+    def add_dataframe(self, df: pd.DataFrame, transfer_type: str) -> str:
         if not df.empty:
             if transfer_type == "file" and self.recent_file_df is not None:
                 print("Predicting files...")
@@ -17,7 +17,8 @@ class Predict:
                 bool_sources = [(True, i) for i, item in enumerate(source_directories) if item in self.recent_file_df.iloc[:, 0]]
                 bool_dests = [(True, i) for i, item in enumerate(dest_directories) if item in self.recent_file_df.iloc[:, 1]]
 
-                return bool_sources, bool_dests
+                if bool_sources or bool_dests:
+                    return f"Sources Predictions: {bool_sources}, Destinations Predictions: {bool_dests}"
 
             elif transfer_type == "network" and self.recent_network_df is not None:
                 print("Predicting network transfers...")
@@ -30,15 +31,17 @@ class Predict:
                 bool_dests = [(True, i) for i, item in enumerate(dest_ip) if item in self.recent_network_df.iloc[:, 1]]
                 bool_ports = [(True, i) for i, item in enumerate(dest_port) if item in self.recent_network_df.iloc[:, 2]]
 
-                return bool_sources, bool_dests, bool_ports
+                return f"Sources Predictions: {bool_sources}, Destinations Predictions: {bool_dests}, Ports Predictions: {bool_ports}"
 
             elif transfer_type == "file" and self.recent_file_df is None:
                 print("No recent file transfers to predict, saved dataframe for future comparisons")
                 self.update_recent_transfers(df, transfer_type)
+                return "No recent file transfers to predict"
 
             elif transfer_type == "network" and self.recent_network_df is None:
                 print("No recent network transfers to predict, saved dataframe for future comparisons")
                 self.update_recent_transfers(df, transfer_type)
+                return "No recent network transfers to predict"
 
         else:
             print("No files to predict")
